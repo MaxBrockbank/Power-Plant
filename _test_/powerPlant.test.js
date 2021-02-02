3a52623bcbfbe7553dc7ed8195f1d69e615aebc0
Max Brockbank <maxbrockbank1999@gmail.com>
2021-02-02 14:27:38 -0800

    pass state checking tests and write storeState function

--- _test_/powerPlant.test.js
+++ _test_/powerPlant.test.js
@@ -1,8 +1,27 @@
-import { hydrate } from './../src/js/powerPlant.js';
+import { changeState } from './../src/js/powerPlant.js';
+import { stateControl } from './../src/js/powerPlant.js';
+import { blueFood } from './../src/js/powerPlant.js';
 
-describe('hydrate', () => {
+describe('changeState', () => {
+  let plant;
+
+  beforeEach(() => {
+    plant = {};
+  });
 
   test('test empty function', () => {
-    expect(hydrate()).toEqual({ soil: 0, light: 0, water: 1});
-  })
+    expect(changeState("water")(1)(plant)).toEqual({ water: 1});
+  });
+});
+
+describe('stateControl', () => {
+  let plant;
+
+  test('test for current state', () => {
+    expect(stateControl()).toEqual({"soil":5, "water":1, "light":0});
+  });
+
+  test('test for changed state', () => {
+    expect(stateControl(blueFood)).toEqual({"soil":10, "water":1, "light":0});
+  });
 });
--- src/js/powerPlant.js
+++ src/js/powerPlant.js
@@ -1,4 +1,47 @@
-export const hydrate = () => {
-  return {};
-  
+/*
+export const hydrate = (plant) => {
+  return {
+    ...plant,
+    water: (plant.water || 0) + 1
+  }
 };
+
+export const feed = (plant) => {
+  return {
+    ...plant,
+    soil: (plant.soil || 0) + 1
+  }
+}
+
+export const giveLight = (plant) => {
+  return {
+    ...plant,
+    light: (plant.light || 0) + 1
+  }
+}
+*/
+
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+export const blueFood = changeState("soil")(5);
+const greenFood = changeState("soil")(10);
+const yuckyFood = changeState("soil")(-5);
+
+const storeState = (initalState) => {
+  let currentState = initalState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+export const stateControl = storeState({"soil":5, "water":1, "light":0});
+
